6ce37e565570fdd4c30e4b96430b4ba9394a8909
Neo Salmon <52429194+jds691@users.noreply.github.com>
2025-10-09 18:51:27 +0100

    Fix Attribute Graph cycle in `MyStudiesView`
    Caused by `MyStudiesModuleCard` using a `GeometryReader` to adjust image dimensions for hit testing.

--- App/MyStudies/MyStudiesModuleCard.swift
+++ App/MyStudies/MyStudiesModuleCard.swift
@@ -18,50 +18,48 @@ struct MyStudiesModuleCard: View {
     @State private var isFavourite: Bool = false
     
     var body: some View {
-        GeometryReader { proxy in
-            ZStack(alignment: .bottomLeading) {
-                ModuleImageView(image: image) { result in
-                    result
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: proxy.size.width, height: proxy.size.height)
-                        .clipped()
-                        //.glur(radius: 3.0)
-                        .clipShape(RoundedRectangle(cornerRadius: 16))
-                        .contentShape(RoundedRectangle(cornerRadius: 16))
-                }
-                
-                HStack(alignment: .bottom) {
-                    VStack(alignment: .leading) {
-                        Text(displayId)
-                            .foregroundStyle(.white)
-                        Text(name)
-                            .font(.title3.bold())
-                            .foregroundStyle(.white)
-                            .lineLimit(2)
-                            .multilineTextAlignment(.leading)
-                    }
-                    Spacer()
-                    Button {
-                        isFavourite.toggle()
-                    } label: {
-                        Label("Mark as favourite", systemImage: isFavourite ? "star.fill" : "star")
-                    }
-                    .buttonStyle(.borderless)
-                    .symbolEffect(.bounce, value: isFavourite)
-                    .imageScale(.large)
-                    .foregroundStyle(.white)
-                    .labelStyle(.iconOnly)
-                    .sensoryFeedback(.success, trigger: isFavourite)
-                }
-                .scenePadding()
+        ZStack(alignment: .bottomLeading) {
+            ModuleImageView(image: image) { result in
+                result
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(
+                        minWidth: 0,
+                        maxWidth: .infinity,
+                        minHeight: 0,
+                        maxHeight: .infinity
+                    )
+                    .aspectRatio(aspectRatio, contentMode: .fit)
+                    .clipped()
+                    .clipShape(RoundedRectangle(cornerRadius: 16))
+                    .contentShape(RoundedRectangle(cornerRadius: 16))
             }
+
+            HStack(alignment: .bottom) {
+                VStack(alignment: .leading) {
+                    Text(displayId)
+                        .foregroundStyle(.white)
+                    Text(name)
+                        .font(.title3.bold())
+                        .foregroundStyle(.white)
+                        .lineLimit(2)
+                        .multilineTextAlignment(.leading)
+                }
+                Spacer()
+                Button {
+                    isFavourite.toggle()
+                } label: {
+                    Label("Mark as favourite", systemImage: isFavourite ? "star.fill" : "star")
+                }
+                .buttonStyle(.borderless)
+                .symbolEffect(.bounce, value: isFavourite)
+                .imageScale(.large)
+                .foregroundStyle(.white)
+                .labelStyle(.iconOnly)
+                .sensoryFeedback(.success, trigger: isFavourite)
+            }
+            .scenePadding()
         }
-        .aspectRatio(aspectRatio, contentMode: .fit)
-        /*.accessibilityElement(children: .ignore)
-        .accessibilityLabel(Text("Class"))
-        .accessibilityValue(Text("Class Name"))
-        .accessibilityAddTraits(.isButton)*/
     }
     
     private var aspectRatio: CGFloat {
