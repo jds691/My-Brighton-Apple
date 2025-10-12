84dadce61bb657ffb34b7cba81eeafe987c160ee
Neo Salmon <52429194+jds691@users.noreply.github.com>
2025-10-12 17:31:16 +0100

    Make Course and Term initialisers nullable
    This reduces the risk of crashing by checking all required fields are present during construction and allows the consumer how to handle invalid objects instead of causing fatal errors.

--- LearnKit/Models/Course.swift
+++ LearnKit/Models/Course.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import os
 
 /*
  (V3 API)
@@ -27,6 +28,8 @@ import Foundation
 */
 
 public struct Course: Hashable, Identifiable, Sendable {
+    private static let logger = Logger(subsystem: "com.neo.LearnKit", category: "Course")
+
     public let id: String
     public let uuid: UUID?
     public let externalId: String?
@@ -50,8 +53,36 @@ public struct Course: Hashable, Identifiable, Sendable {
     public let externalAccessUrl: URL
     public let guestAccessUrl: URL?
 
-    init(from courseSchema: Components.Schemas.Course) {
-        self.id = courseSchema.id!
+    init?(from courseSchema: Components.Schemas.Course) {
+        guard
+            // Course Fields
+            let id = courseSchema.id,
+            let courseId = courseSchema.courseId,
+            let name = courseSchema.name,
+            let modified = courseSchema.modified,
+            let organization = courseSchema.organization,
+            let ultraStatus = courseSchema.ultraStatus,
+            let closedComplete = courseSchema.closedComplete,
+            let termId = courseSchema.termId, // ?
+            let availability = courseSchema.availability,
+            let enrollment = courseSchema.enrollment,
+            let locale = courseSchema.locale,
+            let externalAccessUrl = courseSchema.externalAccessUrl,
+
+            // Required Data Models
+            let availabilityModel = Availability(from: availability),
+            let enrollmentModel = Enrollment(from: enrollment),
+            let localeSettingsModel = LocaleSettings(from: locale)
+        else {
+            Self.logger.error("courseSchema is missing minimum required fields, unable to construt data model.")
+            #if DEBUG
+            dump(courseSchema)
+            #endif
+
+            return nil
+        }
+
+        self.id = id
         if let schemaUuid = courseSchema.uuid {
             self.uuid = UUID(uuidString: schemaUuid)
         } else {
@@ -59,23 +90,32 @@ public struct Course: Hashable, Identifiable, Sendable {
         }
         self.externalId = courseSchema.externalId
         self.dataSourceId = courseSchema.dataSourceId
-        self.courseId = courseSchema.courseId!
-        self.name = courseSchema.name!
+        self.courseId = courseId
+        self.name = name
         self.description = courseSchema.description
         self.creationDate = courseSchema.created
-        self.lastModified = courseSchema.modified!
-        self.isOrganisation = courseSchema.organization!
-        self.ultraStatus = UltraStatus(from: courseSchema.ultraStatus!)
+        self.lastModified = modified
+        self.isOrganisation = organization
+        self.ultraStatus = UltraStatus(from: ultraStatus)
         self.allowGuests = courseSchema.allowGuests
         self.allowObservers = courseSchema.allowObservers
-        self.isComplete = courseSchema.closedComplete!
-        self.termId = courseSchema.termId! // ?
-        self.availability = Availability(from: courseSchema.availability!)
-        self.enrollmentType = Enrollment(from: courseSchema.enrollment!)
-        self.localeSettings = LocaleSettings(from: courseSchema.locale!)
+        self.isComplete = closedComplete
+        self.termId = termId
+        self.availability = availabilityModel
+        self.enrollmentType = enrollmentModel
+        self.localeSettings = localeSettingsModel
         self.hasChildren = courseSchema.hasChildren
         self.parentId = courseSchema.parentId
-        self.externalAccessUrl = URL(string: courseSchema.externalAccessUrl!)!
+        if let externalAccessURL = URL(string: externalAccessUrl) {
+            self.externalAccessUrl = externalAccessURL
+        } else {
+            Self.logger.error("courseSchema.externalAccessUrl is malformed, unable to construct data model.")
+            #if DEBUG
+            dump(externalAccessUrl)
+            #endif
+            return nil
+        }
+
         self.guestAccessUrl = URL(string: courseSchema.guestAccessUrl ?? "")
     }
 
@@ -104,16 +144,37 @@ public struct Course: Hashable, Identifiable, Sendable {
         case selfEnrollment(enrollmentStart: Date, enrollmentEnd: Date, accessCode: String?)
         case emailEnrollment
 
-        init(from enrollmentSchema: Components.Schemas.Course.EnrollmentPayload) {
-            switch enrollmentSchema._type {
+        init?(from enrollmentSchema: Components.Schemas.Course.EnrollmentPayload) {
+            guard
+                let enrollmentType = enrollmentSchema._type
+            else {
+                Course.logger.error("enrollmentSchema is missing minimum required fields, unable to construt data model.")
+#if DEBUG
+                dump(enrollmentSchema)
+#endif
+
+                return nil
+            }
+
+            switch enrollmentType {
                 case .selfEnrollment:
-                    self = .selfEnrollment(enrollmentStart: enrollmentSchema.start!, enrollmentEnd: enrollmentSchema.end!, accessCode: enrollmentSchema.accessCode!)
+                    guard
+                        let startDate = enrollmentSchema.start,
+                        let endDate = enrollmentSchema.end
+                    else {
+                        Course.logger.error("enrollmentSchema is SelfEnrollment but is missing either `start` or `end` fields, unable to construt data model.")
+#if DEBUG
+                        dump(enrollmentSchema)
+#endif
+
+                        return nil
+                    }
+
+                    self = .selfEnrollment(enrollmentStart: startDate, enrollmentEnd: endDate, accessCode: enrollmentSchema.accessCode)
                 case .emailEnrollment:
                     self = .emailEnrollment
                 case .instructorLed:
                     self = .instructorLed
-                default:
-                    fatalError()
             }
         }
     }
@@ -122,9 +183,26 @@ public struct Course: Hashable, Identifiable, Sendable {
         public let status: Self.Status
         public let duration: Self.Duration
 
-        init(from availabilitySchema: Components.Schemas.Course.AvailabilityPayload) {
-            self.status = Status(rawValue: availabilitySchema.available!.rawValue)!
-            self.duration = Duration(from: availabilitySchema.duration!)
+        init?(from availabilitySchema: Components.Schemas.Course.AvailabilityPayload) {
+            guard
+                // Availability Fields
+                let status = availabilitySchema.available,
+                let duration = availabilitySchema.duration,
+
+                // Required Data Models
+                let statusModel = Status(rawValue: status.rawValue),
+                let durationModel = Duration(from: duration)
+            else {
+                Course.logger.error("availabilitySchema is missing minimum required fields, unable to construt data model.")
+#if DEBUG
+                dump(availabilitySchema)
+#endif
+
+                return nil
+            }
+
+            self.status = statusModel
+            self.duration = durationModel
         }
 
         public enum Status: String, RawRepresentable, Hashable, Sendable {
@@ -140,18 +218,50 @@ public struct Course: Hashable, Identifiable, Sendable {
             case numberOfDays(_ days: Int)
             case inheritFromTerm
 
-            init(from durationSchema: Components.Schemas.Course.AvailabilityPayload.DurationPayload) {
-                switch durationSchema._type {
+            init?(from durationSchema: Components.Schemas.Course.AvailabilityPayload.DurationPayload) {
+                guard
+                    let durationType = durationSchema._type
+                else {
+                    Course.logger.error("durationSchema is missing minimum required fields, unable to construt data model.")
+#if DEBUG
+                    dump(durationSchema)
+#endif
+
+                    return nil
+                }
+
+                switch durationType {
                     case .continuous:
                         self = .continuous
                     case .dateRange:
-                        self = .dateRange(start: durationSchema.start!, end: durationSchema.end!)
+                        guard
+                            let startDate = durationSchema.start,
+                            let endDate = durationSchema.end
+                        else {
+                            Course.logger.error("enrollmentSchema is DateRange but is missing either `start` or `end` fields, unable to construt data model.")
+#if DEBUG
+                            dump(durationSchema)
+#endif
+
+                            return nil
+                        }
+
+                        self = .dateRange(start: startDate, end: endDate)
                     case .fixedNumDays:
-                        self = .numberOfDays(Int(durationSchema.daysOfUse!))
+                        guard
+                            let days = durationSchema.daysOfUse
+                        else {
+                            Course.logger.error("enrollmentSchema is NumberOfDays but is missing `daysOfUse` field, unable to construt data model.")
+#if DEBUG
+                            dump(durationSchema)
+#endif
+
+                            return nil
+                        }
+
+                        self = .numberOfDays(Int(days))
                     case .useTerm:
                         self = .inheritFromTerm
-                    default:
-                        fatalError()
                 }
             }
         }
@@ -161,9 +271,20 @@ public struct Course: Hashable, Identifiable, Sendable {
         public let identifier: String?
         public let forceLocale: Bool
 
-        init(from localeSchema: Components.Schemas.Course.LocalePayload) {
+        init?(from localeSchema: Components.Schemas.Course.LocalePayload) {
+            guard
+                let forceLocale = localeSchema.force
+            else {
+                Course.logger.error("localeSchema is missing minimum required fields, unable to construt data model.")
+#if DEBUG
+                dump(localeSchema)
+#endif
+
+                return nil
+            }
+
             self.identifier = localeSchema.id
-            self.forceLocale = localeSchema.force!
+            self.forceLocale = forceLocale
         }
     }
 }
--- LearnKit/Models/Term.swift
+++ LearnKit/Models/Term.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import os
 
 /*
  (V3 API)
@@ -16,6 +17,8 @@ import Foundation
  */
 
 public struct Term: Hashable, Identifiable, Sendable {
+    private static let logger = Logger(subsystem: "com.neo.LearnKit", category: "Term")
+
     public let id: String
     public let externalId: String?
     public let dataSourceId: String?
@@ -23,22 +26,55 @@ public struct Term: Hashable, Identifiable, Sendable {
     public let description: String?
     public let availability: Term.Availability
 
-    init(from termSchema: Components.Schemas.Term) {
-        self.id = termSchema.id!
+    init?(from termSchema: Components.Schemas.Term) {
+        guard
+            // Term Fields
+            let id = termSchema.id,
+            let name = termSchema.name,
+            let availability = termSchema.availability,
+
+            // Required Data Models
+            let availabilityModel = Availability(from: availability)
+        else {
+            Self.logger.error("termSchema is missing minimum required fields, unable to construt data model.")
+#if DEBUG
+            dump(termSchema)
+#endif
+
+            return nil
+        }
+
+        self.id = id
         self.externalId = termSchema.externalId
         self.dataSourceId = termSchema.dataSourceId
-        self.name = termSchema.name!
+        self.name = name
         self.description = termSchema.description
-        self.availability = Availability(from: termSchema.availability!)
+        self.availability = availabilityModel
     }
 
     public struct Availability: Hashable, Sendable {
         public let isAvailable: Bool
         public let duration: Availability.Duration
 
-        init(from termAvailabilitySchema: Components.Schemas.Term.AvailabilityPayload) {
-            self.isAvailable = termAvailabilitySchema.available! == .yes
-            self.duration = Availability.Duration(from: termAvailabilitySchema.duration!)
+        init?(from termAvailabilitySchema: Components.Schemas.Term.AvailabilityPayload) {
+            guard
+                // Availability Fields
+                let available = termAvailabilitySchema.available,
+                let duration = termAvailabilitySchema.duration,
+
+                // Required Data Models
+                let durationModel = Availability.Duration(from: duration)
+            else {
+                Term.logger.error("termAvailabilitySchema is missing minimum required fields, unable to construt data model.")
+#if DEBUG
+                dump(termAvailabilitySchema)
+#endif
+
+                return nil
+            }
+
+            self.isAvailable = available == .yes
+            self.duration = durationModel
         }
 
         public enum Duration: Hashable, Sendable {
@@ -46,14 +82,48 @@ public struct Term: Hashable, Identifiable, Sendable {
             case dateRange(start: Date, end: Date)
             case numberOfDays(_ days: Int)
 
-            init(from termAvailabilityDurationSchema: Components.Schemas.Term.AvailabilityPayload.DurationPayload) {
-                switch termAvailabilityDurationSchema._type! {
+            init?(from termAvailabilityDurationSchema: Components.Schemas.Term.AvailabilityPayload.DurationPayload) {
+                guard
+                    let durationType = termAvailabilityDurationSchema._type
+                else {
+                    Term.logger.error("termAvailabilityDurationSchema is missing minimum required fields, unable to construt data model.")
+#if DEBUG
+                    dump(termAvailabilityDurationSchema)
+#endif
+
+                    return nil
+                }
+
+                switch durationType {
                     case .continuous:
                         self = .continuous
                     case .dateRange:
-                        self = .dateRange(start: termAvailabilityDurationSchema.start!, end: termAvailabilityDurationSchema.end!)
+                        guard
+                            let startDate = termAvailabilityDurationSchema.start,
+                            let endDate = termAvailabilityDurationSchema.end
+                        else {
+                            Term.logger.error("termAvailabilityDurationSchema is DateRange but is missing either `start` or `end` fields, unable to construt data model.")
+#if DEBUG
+                            dump(termAvailabilityDurationSchema)
+#endif
+
+                            return nil
+                        }
+
+                        self = .dateRange(start: startDate, end: endDate)
                     case .fixedNumDays:
-                        self = .numberOfDays(Int(termAvailabilityDurationSchema.daysOfUse!))
+                        guard
+                            let days = termAvailabilityDurationSchema.daysOfUse
+                        else {
+                            Term.logger.error("termAvailabilityDurationSchema is NumberOfDays but is missing `daysOfUse` field, unable to construt data model.")
+#if DEBUG
+                            dump(termAvailabilityDurationSchema)
+#endif
+
+                            return nil
+                        }
+
+                        self = .numberOfDays(Int(days))
                 }
             }
         }
