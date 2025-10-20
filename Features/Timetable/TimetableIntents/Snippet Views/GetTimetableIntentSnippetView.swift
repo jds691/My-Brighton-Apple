3892d2eab6359c9c00a26fcdfb41a104c462d4ee
Neo Salmon <52429194+jds691@users.noreply.github.com>
2025-10-20 20:00:00 +0100

    Restore iOS/macOS 26 Snippet View theming
    Still needs adjusted and resized

--- Features/Timetable/TimetableIntents/Snippet Views/GetTimetableIntentSnippetView.swift
+++ Features/Timetable/TimetableIntents/Snippet Views/GetTimetableIntentSnippetView.swift
@@ -49,21 +49,21 @@ public struct GetTimetableIntentSnippetView: View {
                 .fixedSize(horizontal: false, vertical: true)
             }
         }
-        /*.modifierBranch {
+        .modifierBranch {
             if #available(iOS 26, macOS 26, *) {
                 $0
                     .scenePadding()
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .background(.brightonBackground)
                     .clipShape(ContainerRelativeShape())
-                    .scenePadding(.horizontal)
+                    .scenePadding([.horizontal, .top])
                     .foregroundStyle(.primary, .brightonSecondary)
             } else {
                 $0
                     .scenePadding(.horizontal)
                     .frame(maxWidth: .infinity, alignment: .leading)
             }
-        }*/
+        }
     }
 }
 
---
+++ Features/Timetable/TimetableIntents/Snippet Views/ModifierBranch.swift
@@ -0,0 +1,29 @@
+//
+//  ModifierBranch.swift
+//  My Brighton
+//
+//  Created by Neo Salmon on 07/07/2025.
+//
+
+import SwiftUI
+
+// TODO: De-duplicate
+
+struct ModifierBranchViewModifier<Result: View>: ViewModifier {
+    let modifiers: (Content) -> Result
+
+    init(modifiers: @escaping (Content) -> Result) {
+        self.modifiers = modifiers
+    }
+
+    func body(content: Content) -> Result {
+        modifiers(content)
+    }
+}
+
+extension View {
+    func modifierBranch(@ViewBuilder _ modifiers: @escaping (Self) -> some View) -> some View {
+        //modifier(ModifierBranchViewModifier(modifiers: modifiers))
+        modifiers(self)
+    }
+}
