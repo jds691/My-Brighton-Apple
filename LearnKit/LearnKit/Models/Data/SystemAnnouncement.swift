9d06812733de3b4ce3abd5f9627bea3e515b91ad
Neo Salmon <52429194+jds691@users.noreply.github.com>
2026-02-13 18:07:40 +0000

    Finish domain model for system announcment
    DocC comments still required but eh

--- LearnKit/LearnKit/Models/Data/SystemAnnouncement.swift
+++ LearnKit/LearnKit/Models/Data/SystemAnnouncement.swift
@@ -6,8 +6,12 @@
 //
 
 import Foundation
+import os
 
-public struct SystemAnnouncement: Hashable, Identifiable {
+// TODO: DocC comments
+public struct SystemAnnouncement: Hashable, Identifiable, Sendable {
+    private static let logger = Logger(subsystem: "com.neo.LearnKit", category: "SystemAnnouncement")
+
     public var id: String
     public var title: String
     public var body: String
@@ -18,21 +22,76 @@ public struct SystemAnnouncement: Hashable, Identifiable {
     public var creationDate: Date
     public var lastModified: Date
 
-    init(apiSchema: Components.Schemas.SystemAnnouncement) {
-        // TODO: Fix the OpenAPI docs
-        self.id = apiSchema.id!
-        self.title = apiSchema.title!
-        self.body = apiSchema.body!
-        self.availability = .permenant
-        self.showAtLogin = apiSchema.showAtLogin!
-        self.showInCourses = apiSchema.showInCourses!
-        self.creatorID = apiSchema.creatorUserId!
-        self.creationDate = apiSchema.created!
-        self.lastModified = apiSchema.modified!
+    // On My Brighton no system announcements are actually available, so it's not possible to see the default fields.
+    // These requirements are modelled after course announcements
+    init?(systemAnnouncementSchema: Components.Schemas.SystemAnnouncement) {
+        guard
+            // System Announcement Fields
+            let id = systemAnnouncementSchema.id,
+            let title = systemAnnouncementSchema.title,
+            let body = systemAnnouncementSchema.body,
+            let availability = systemAnnouncementSchema.availability,
+            let creatorId = systemAnnouncementSchema.creatorUserId,
+            let creationDate = systemAnnouncementSchema.created,
+            let lastModified = systemAnnouncementSchema.modified,
+
+            // Required Data Models
+            let availabilityModel = Availability(from: availability)
+        else {
+            Self.logger.error("systemAnnouncementSchema is missing minimum required fields, unable to construt data model.")
+#if DEBUG
+            dump(systemAnnouncementSchema)
+#endif
+
+            return nil
+        }
+        
+        self.id = id
+        self.title = title
+        self.body = body
+        self.availability = availabilityModel
+        self.showAtLogin = systemAnnouncementSchema.showAtLogin ?? false
+        self.showInCourses = systemAnnouncementSchema.showInCourses ?? false
+        self.creatorID = creatorId
+        self.creationDate = creationDate
+        self.lastModified = lastModified
     }
 
-    public enum Availability: Hashable {
+    public enum Availability: Hashable, Sendable {
         case permenant
-        case duration(start: Date, end: Date)
+        case restricted(start: Date, end: Date)
+
+        init?(from systemAnnouncementAvailabilitySchema: Components.Schemas.SystemAnnouncement.AvailabilityPayload) {
+            guard
+                let duration = systemAnnouncementAvailabilitySchema.duration,
+                let durationType = duration._type
+            else {
+                SystemAnnouncement.logger.error("systemAnnouncementAvailabilitySchema is missing minimum required fields, unable to construt data model.")
+#if DEBUG
+                dump(systemAnnouncementAvailabilitySchema)
+#endif
+
+                return nil
+            }
+
+            switch durationType {
+                case .permanent:
+                    self = .permenant
+                case .restricted:
+                    guard
+                        let startDate = duration.start,
+                        let endDate = duration.end
+                    else {
+                        SystemAnnouncement.logger.error("systemAnnouncementAvailabilitySchema is Restricted but is missing either `start` or `end` fields, unable to construt data model.")
+#if DEBUG
+                        dump(systemAnnouncementAvailabilitySchema)
+#endif
+
+                        return nil
+                    }
+
+                    self = .restricted(start: startDate, end: endDate)
+            }
+        }
     }
 }
