805dae01002777a76f33c37714a0389c65fb1d43
Neo Salmon <52429194+jds691@users.noreply.github.com>
2026-03-25 16:13:42 +0000

    Connect DashboardKit to the main app

---
+++ App/DashboardKit+Environment.swift
@@ -0,0 +1,13 @@
+//
+//  DashboardKit+Environment.swift
+//  My Brighton
+//
+//  Created by Neo Salmon on 25/03/2026.
+//
+
+import SwiftUI
+import DashboardKit
+
+extension EnvironmentValues {
+    @Entry var dashboardService: DashboardService = DashboardService(dashboards: [])
+}
