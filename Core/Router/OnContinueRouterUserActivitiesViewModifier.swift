369369dc37e79c4b40dce08487ab5f12586dfdac
Neo Salmon <52429194+jds691@users.noreply.github.com>
2026-05-04 15:01:21 +0100

    Fix spotlight continuation for LearnKit content

--- Core/Router/OnContinueRouterUserActivitiesViewModifier.swift
+++ Core/Router/OnContinueRouterUserActivitiesViewModifier.swift
@@ -12,8 +12,15 @@ import LearnKit
 public struct OnContinueRouterUserActivitiesViewModifier: ViewModifier {
     @Environment(\.openWindow) private var openWindow
     @Environment(\.supportsMultipleWindows) private var supportsMultipleWindows
+    @Environment(\.openURL) private var openURL
 
     @Environment(Router.self) private var router
+
+    private let learnKit: LearnKitService
+
+    init(learnKit: LearnKitService) {
+        self.learnKit = learnKit
+    }
 
     public func body(content: Self.Content) -> some View {
         content
@@ -27,7 +34,11 @@ public struct OnContinueRouterUserActivitiesViewModifier: ViewModifier {
             .onContinueUserActivity(CSSearchableItemActionType) { activity in
                 guard let itemIdentifier = activity.userInfo?[CSSearchableItemActivityIdentifier] as? String else { return }
 
-                if let route = Navigation.Route(spotlightIdentifier: itemIdentifier) {
+                if itemIdentifier.starts(with: "content/") {
+                    Task {
+                        await handleLearnKitContentNavigation(for: itemIdentifier)
+                    }
+                } else if let route = Navigation.Route(spotlightIdentifier: itemIdentifier) {
                     router.navigate(to: .route(route))
                 }
             }
@@ -59,10 +70,57 @@ public struct OnContinueRouterUserActivitiesViewModifier: ViewModifier {
                 }
             }
     }
+
+    private func handleLearnKitContentNavigation(for itemIdentifier: String) async {
+        let components = itemIdentifier.split(separator: "/")
+        guard components.count >= 3 else { return }
+
+        let courseId: Course.ID = String(components[1])
+
+        guard let content = try? await learnKit.getContent(for: String(components[2]), in: courseId) else { return }
+
+        switch content.handler {
+            case .contentItem, .contentFolder(isBbPage: _), .contentLesson:
+                router.navigate(to: .route(.myStudies(.module(courseId, .content(content.id)))))
+            case .assignment(gradeColumn: let gradeColumnId, isGroup: _), .testLink(target: _, gradeColumn: let gradeColumnId):
+                router.navigate(to: .route(.myStudies(.module(courseId, .grades(gradeColumnId)))))
+            case .externalLink(let url):
+                if #available(iOS 26, macOS 26, *) {
+                    openURL(url, prefersInApp: true)
+                } else {
+                    openURL(url)
+                }
+            case .ltiLink(let url, parameters: let customParams):
+                if var components = URLComponents(string: url.absoluteString) {
+                    components.queryItems = customParams.map {
+                        URLQueryItem(name: $0, value: $1)
+                    }
+
+                    if #available(iOS 26, macOS 26, *) {
+                        openURL(url, prefersInApp: true)
+                    } else {
+                        openURL(url)
+                    }
+                } else {
+                    return
+                }
+            default:
+                if !content.links.isEmpty {
+                    let resolvedUrl = URL(string: "https://studentcentral.brighton.ac.uk")!.appending(path: content.links.first!.href)
+                    if #available(iOS 26, macOS 26, *) {
+                        openURL(resolvedUrl, prefersInApp: true)
+                    } else {
+                        openURL(resolvedUrl)
+                    }
+                } else {
+                    return
+                }
+        }
+    }
 }
 
 public extension View {
-    func onContinueRouterUserActivities() -> some View {
-        modifier(OnContinueRouterUserActivitiesViewModifier())
+    func onContinueRouterUserActivities(learnKit: LearnKitService) -> some View {
+        modifier(OnContinueRouterUserActivitiesViewModifier(learnKit: learnKit))
     }
 }
